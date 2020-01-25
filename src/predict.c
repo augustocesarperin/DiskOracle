7e04e7276a4704f70f9d2d3df69d178bdec55ae5
Augusto César Perin <augustocesarperin@gmail.com>
2020-01-25 12:34:00 -0300

    Predict

--- src/predict.c
+++ src/predict.c
@@ -1,5 +1,21 @@
 #include "predict.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
 int predict_failure(const char *device) {
-    return 0;
+    srand((unsigned)time(NULL));
+    int risk = rand() % 100;
+    if (risk < 10) {
+        printf("Prediction: FAILURE IMMINENT for %s\n", device);
+        return 2;
+    } else if (risk < 30) {
+        printf("Prediction: WARNING for %s\n", device);
+        return 1;
+    } else {
+        printf("Prediction: OK for %s\n", device);
+        return 0;
+    }
 }
