41f051946705271c93dbc3cd74225c32adef2c1c
Augusto César Perin <augustocesarperin@gmail.com>
2020-01-29 09:36:54 -0300

    Implement Backup

--- src/surface.c
+++ src/surface.c
@@ -6,6 +6,23 @@
 #include <stdint.h>
 #include <time.h>
 
+static void backup_critical_sectors(const char *device) {
+    int fd = open(device, O_RDONLY);
+    if (fd < 0) return;
+    uint8_t buf[1024];
+    ssize_t r = read(fd, buf, sizeof(buf));
+    close(fd);
+    if (r != sizeof(buf)) return;
+    const char *dev = strrchr(device, '/');
+    if (!dev) dev = device; else dev++;
+    char fname[256];
+    snprintf(fname, sizeof(fname), "backup/%s_mbr_vbr.bin", dev);
+    FILE *f = fopen(fname, "wb");
+    if (!f) return;
+    fwrite(buf, 1, sizeof(buf), f);
+    fclose(f);
+}
+
 int surface_scan_quick(const char *device) {
     int fd = open(device, O_RDONLY | O_DIRECT);
     if (fd < 0) {
@@ -29,6 +46,7 @@ int surface_scan_quick(const char *device) {
 }
 
 int surface_scan_deep(const char *device) {
+    backup_critical_sectors(device);
     int fd = open(device, O_RDONLY | O_DIRECT);
     if (fd < 0) {
         perror("open");
