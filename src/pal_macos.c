70043dd98e1bec68400b600aa5d4084d1ebdd28f
Augusto César Perin <augustocesarperin@gmail.com>
2020-02-02 11:40:54 -0300

    Add disk listing

--- src/pal_macos.c
+++ src/pal_macos.c
@@ -1,9 +1,18 @@
 #include "pal.h"
 
+#ifdef __APPLE__
+#include <stdio.h>
+#include <stdlib.h>
 int pal_list_drives() {
+    printf("Discos disponíveis (macOS):\n");
+    system("diskutil list | grep '^/dev/'");
     return 0;
 }
-
 int pal_get_smart(const char *device) {
-    return 0;
+    printf("SMART em macOS não implementado nesta versão\n");
+    return 1;
 }
+#else
+int pal_list_drives() { printf("PAL macOS não disponível neste SO\n"); return 1; }
+int pal_get_smart(const char *device) { return 1; }
+#endif
--- src/pal_windows.c
+++ src/pal_windows.c
@@ -1,9 +1,22 @@
 #include "pal.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#include <stdio.h>
 int pal_list_drives() {
+    DWORD drives = GetLogicalDrives();
+    printf("Unidades disponíveis:\n");
+    for (char d = 'A'; d <= 'Z'; ++d) {
+        if (drives & (1 << (d - 'A')))
+            printf("- %c:\\\n", d);
+    }
     return 0;
 }
-
 int pal_get_smart(const char *device) {
-    return 0;
+    printf("SMART em Windows não implementado nesta versão\n");
+    return 1;
 }
+#else
+int pal_list_drives() { printf("PAL Windows não disponível neste SO\n"); return 1; }
+int pal_get_smart(const char *device) { return 1; }
+#endif
