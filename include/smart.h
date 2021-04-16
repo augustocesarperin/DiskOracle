811d387bbb0d8767240ce0386e58c66e7633e59b
Augusto César Perin <augustocesarperin@gmail.com>
2021-04-16 11:05:34 -0300

    Implement real SMART analysis for NVMe SSDs on Linux

--- include/smart.h
+++ include/smart.h
@@ -1,7 +1,41 @@
 #ifndef SMART_H
 #define SMART_H
 
-int smart_read(const char *device);
-int smart_interpret(const char *device);
+#include <stdint.h>
+
+struct smart_attr {
+    uint8_t id;
+    uint16_t flags;
+    uint8_t value;
+    uint8_t worst;
+    uint8_t raw[6];
+};
+
+struct smart_nvme {
+    uint8_t critical_warning;
+    uint16_t temperature;
+    uint8_t avail_spare;
+    uint8_t spare_thresh;
+    uint8_t percent_used;
+    uint64_t data_units_read;
+    uint64_t data_units_written;
+    uint64_t host_reads;
+    uint64_t host_writes;
+    uint64_t power_cycles;
+    uint64_t power_on_hours;
+    uint64_t unsafe_shutdowns;
+    uint64_t media_errors;
+    uint64_t num_err_log_entries;
+};
+
+struct smart_data {
+    struct smart_attr attrs[30];
+    int attr_count;
+    struct smart_nvme nvme;
+    int is_nvme;
+};
+
+int smart_read(const char *device, struct smart_data *out);
+int smart_interpret(const char *device, struct smart_data *data);
 
 #endif
--- src/smart.c
+++ src/smart.c
@@ -20,13 +20,10 @@ static int is_nvme(const char *device) {
     return strstr(device, "nvme") != NULL;
 }
 
-int smart_read(const char *device) {
+int smart_read(const char *device, struct smart_data *out) {
     if (is_nvme(device)) {
         int fd = open(device, O_RDWR);
-        if (fd < 0) {
-            perror("open");
-            return 1;
-        }
+        if (fd < 0) return 1;
         unsigned char nvme_log[512];
         memset(nvme_log, 0, sizeof(nvme_log));
         struct {
@@ -48,21 +45,29 @@ int smart_read(const char *device) {
             __u32 result;
         } cmd;
         memset(&cmd, 0, sizeof(cmd));
-        cmd.opcode = 0x02; // NVMe Admin Get Log Page
+        cmd.opcode = 0x02;
         cmd.addr = (__u64)(uintptr_t)nvme_log;
         cmd.data_len = sizeof(nvme_log);
         cmd.nsid = 0xFFFFFFFF;
-        cmd.cdw10 = (0x02 << 16) | (sizeof(nvme_log) / 4); // Log page 0x02, length in dwords
-        int err = ioctl(fd, 0xC0484E41, &cmd); // NVME_IOCTL_ADMIN_CMD
-        if (err < 0) {
-            perror("ioctl NVME_IOCTL_ADMIN_CMD");
-            close(fd);
-            return 1;
-        }
-        printf("NVMe SMART/Health Log (first 16 bytes): ");
-        for (int i = 0; i < 16; ++i) printf("%02X ", nvme_log[i]);
-        printf("...\n");
+        cmd.cdw10 = (0x02 << 16) | (sizeof(nvme_log) / 4);
+        int err = ioctl(fd, 0xC0484E41, &cmd);
         close(fd);
+        if (err < 0) return 1;
+        out->is_nvme = 1;
+        out->nvme.critical_warning = nvme_log[0];
+        out->nvme.temperature = nvme_log[1] | (nvme_log[2]<<8);
+        out->nvme.avail_spare = nvme_log[3];
+        out->nvme.spare_thresh = nvme_log[4];
+        out->nvme.percent_used = nvme_log[5];
+        out->nvme.data_units_read = *(uint64_t*)&nvme_log[32];
+        out->nvme.data_units_written = *(uint64_t*)&nvme_log[48];
+        out->nvme.host_reads = *(uint64_t*)&nvme_log[64];
+        out->nvme.host_writes = *(uint64_t*)&nvme_log[80];
+        out->nvme.power_cycles = *(uint64_t*)&nvme_log[96];
+        out->nvme.power_on_hours = *(uint64_t*)&nvme_log[104];
+        out->nvme.unsafe_shutdowns = *(uint64_t*)&nvme_log[112];
+        out->nvme.media_errors = *(uint64_t*)&nvme_log[120];
+        out->nvme.num_err_log_entries = *(uint64_t*)&nvme_log[128];
         return 0;
     }
     int fd = open(device, O_RDWR | O_NONBLOCK);
@@ -97,20 +102,77 @@ int smart_read(const char *device) {
         close(fd);
         return 1;
     }
-    printf("SMART RAW DATA (first 16 bytes): ");
-    for (int i = 0; i < 16; ++i) printf("%02X ", data[i]);
-    printf("...\n");
     close(fd);
+    int count = 0;
+    for (int i = 2; i + 12 <= 362 && count < 30; i += 12) {
+        uint8_t id = data[i];
+        if (id == 0) continue;
+        out->attrs[count].id = id;
+        out->attrs[count].flags = data[i+1] | (data[i+2]<<8);
+        out->attrs[count].value = data[i+3];
+        out->attrs[count].worst = data[i+4];
+        memcpy(out->attrs[count].raw, &data[i+5], 6);
+        count++;
+    }
+    out->attr_count = count;
+    out->is_nvme = 0;
     return 0;
 }
 
-int smart_interpret(const char *device) {
-    printf("SMART interpretation for %s: ", device);
-    if (is_nvme(device)) {
-        printf("NVMe interpretation not implemented yet\n");
-        return 1;
+int smart_interpret(const char *device, struct smart_data *data) {
+    if (data->is_nvme) {
+        printf("Diagnóstico SMART NVMe para %s:\n", device);
+        printf("Critical Warning: 0x%02X\n", data->nvme.critical_warning);
+        printf("Temperature: %u K (%.2f °C)\n", data->nvme.temperature, data->nvme.temperature - 273.15);
+        printf("Available Spare: %u%%\n", data->nvme.avail_spare);
+        printf("Spare Threshold: %u%%\n", data->nvme.spare_thresh);
+        printf("Percentage Used: %u%%\n", data->nvme.percent_used);
+        printf("Power On Hours: %llu\n", data->nvme.power_on_hours);
+        printf("Unsafe Shutdowns: %llu\n", data->nvme.unsafe_shutdowns);
+        printf("Media Errors: %llu\n", data->nvme.media_errors);
+        printf("Data Units Read: %llu\n", data->nvme.data_units_read);
+        printf("Data Units Written: %llu\n", data->nvme.data_units_written);
+        if (data->nvme.percent_used > 90)
+            printf("ALERTA: SSD NVMe próximo do fim da vida útil!\n");
+        if (data->nvme.critical_warning)
+            printf("ALERTA: Critical Warning ativo!\n");
+        if (data->nvme.media_errors > 0)
+            printf("ALERTA: Media Errors detectados!\n");
+        return 0;
     }
-    // Exemplo de interpretação simples
-    printf("OK (demo)\n");
+    printf("Diagnóstico SMART real para %s:\n", device);
+    int reallocated = -1, pending = -1, poweron = -1, offline_unc = -1;
+    for (int i = 0; i < data->attr_count; ++i) {
+        uint8_t id = data->attrs[i].id;
+        uint64_t raw = 0;
+        for (int j = 0; j < 6; ++j) raw |= ((uint64_t)data->attrs[i].raw[j]) << (8*j);
+        if (id == 5) reallocated = raw;
+        if (id == 9) poweron = raw;
+        if (id == 197) pending = raw;
+        if (id == 198) offline_unc = raw;
+        printf("ID %3d | Value %3d | Worst %3d | RAW %-10llu\n", id, data->attrs[i].value, data->attrs[i].worst, raw);
+    }
+    if (reallocated >= 0) {
+        if (reallocated > 10)
+            printf("Setores realocados: %d. ALERTA VERMELHO!\n", reallocated);
+        else if (reallocated > 0)
+            printf("Setores realocados: %d. Atenção!\n", reallocated);
+        else
+            printf("Setores realocados: %d. OK.\n", reallocated);
+    }
+    if (pending >= 0) {
+        if (pending > 0)
+            printf("Pending sectors: %d. ALERTA!\n", pending);
+        else
+            printf("Pending sectors: %d. OK.\n", pending);
+    }
+    if (offline_unc >= 0) {
+        if (offline_unc > 0)
+            printf("Uncorrectable sectors: %d. ALERTA!\n", offline_unc);
+        else
+            printf("Uncorrectable sectors: %d. OK.\n", offline_unc);
+    }
+    if (poweron >= 0)
+        printf("Horas ligadas: %d\n", poweron);
     return 0;
 }
